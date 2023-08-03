3e37341e9be16a28b038aac91a0463be389f42f8
Lovelesh Sharma <lovelesh90@gmail.com>
2023-08-03 21:32:09 +0530

    Adds test file as another test scenario

--- cmd/snakes-and-ladder/main.go
+++ cmd/snakes-and-ladder/main.go
@@ -5,14 +5,8 @@ import (
 	"github.com/loveleshsharma/snakesandladder/internal"
 )
 
-/*
-	PENDING
-	1. input via FILE and CLI
-	2. introduce OOPS concepts
-*/
-
 func main() {
-	game := internal.NewGame(internal.NewBoard(), internal.NewDice(), internal.NewFileReader("./test-files/file1.txt"))
+	game := internal.NewGame(internal.NewBoard(), internal.NewDice(), internal.NewFileReader("./test-files/file2.txt"))
 
 	if err := game.StartGame(); err != nil {
 		fmt.Println(err)
